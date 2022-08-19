e37e570878e23ef60d552440a0f86c807de69b81
douglas <douglasmwangi94@gmail.com>
2022-08-19 20:23:10 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = `roller-skate`){
+
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+
+function wrapAdjective(narration = "special"){
+    const innerFunction = function (etiNini  = "*"){
+        return `You are ${narration}${etiNini }${narration}!`
+    }
+    return innerFunction
+}
